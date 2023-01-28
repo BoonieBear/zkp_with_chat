284b455f425d0559189638aebd0aab0303b6618d
Fu, Xiang <20948660@qq.com>
2023-01-28 23:41:57 +0800

    new helloworld from course.rs

--- rust_study/guessing_game/src/main.rs
+++ rust_study/guessing_game/src/main.rs
@@ -1,6 +1,17 @@
 use std::io;
 use rand::Rng;
+
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
 fn main() {
+    greet_world();
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
